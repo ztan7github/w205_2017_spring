bee646637624a7ad8b5756c636269ac4aee2171e
w205 <w205@ip-172-31-20-97.ec2.internal>
2017-03-26 02:02:50 +0000

    add CLEAN_load_data_lake.sh

--- exercise_1/loading_and_modeling/CLEAN_load_data_lake.sh
+++ exercise_1/loading_and_modeling/CLEAN_load_data_lake.sh
@@ -9,29 +9,30 @@ rmdir ~/staging/exercise_1
 rmdir ~/staging
 
 # remove files from hdfs
-#hdfs dfs -rm /user/w205/hospital_compare/hospitals.csv
-#hdfs dfs -rm /user/w205/hospital_compare/effective_care.csv
-#hdfs dfs -rm /user/w205/hospital_compare/readmissions.csv
-#hdfs dfs -rm /user/w205/hospital_compare/Measures.csv
-#hdfs dfs -rm /user/w205/hospital_compare/surveys_responses.csv
+# hdfs dfs -rm /user/w205/hospital_compare/hospitals.csv
+# hdfs dfs -rm /user/w205/hospital_compare/effective_care.csv
+# hdfs dfs -rm /user/w205/hospital_compare/readmissions.csv
+# hdfs dfs -rm /user/w205/hospital_compare/Measures.csv
+# hdfs dfs -rm /user/w205/hospital_compare/survey_responses.csv
 
 hdfs dfs -rm /user/w205/hospital_compare/hospitals/hospitals.csv
 hdfs dfs -rm /user/w205/hospital_compare/effective_care/effective_care.csv
 hdfs dfs -rm /user/w205/hospital_compare/readmissions/readmissions.csv
-hdfs dfs -rm /user/w205/hospital_compare/Measures/Measures.csv
+hdfs dfs -rm /user/w205/hospital_compare/measures/Measures.csv
 hdfs dfs -rm /user/w205/hospital_compare/surveys_responses/surveys_responses.csv
 
-# remove our hdfs directory
-hdfs dfs -rmdir  /user/w205/hospital_compare/hospitals
-hdfs dfs -rmdir  /user/w205/hospital_compare/effective_care
-hdfs dfs -rmdir  /user/w205/hospital_compare/readmissions
-hdfs dfs -rmdir  /user/w205/hospital_compare/Measures
-hdfs dfs -rmdir  /user/w205/hospital_compare/surveys_responses
+# remove our hdfs directories
+hdfs dfs -rmdir /user/w205/hospital_compare/hospitals
+hdfs dfs -rmdir /user/w205/hospital_compare/effective_care
+hdfs dfs -rmdir /user/w205/hospital_compare/readmissions
+hdfs dfs -rmdir /user/w205/hospital_compare/measures
+hdfs dfs -rmdir /user/w205/hospital_compare/surveys_responses
 
-hdfs dfs -rmdir  /user/w205/hospital_compare/*
+hdfs dfs -rmdir /user/w205/hospital_compare
 
 #change directory back to the original
 cd $MY_CWD
 
 #clean exit
 exit
+
