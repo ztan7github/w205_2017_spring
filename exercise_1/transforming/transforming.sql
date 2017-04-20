0f5358a07a97728049147ad9358d4a4ed69dc686
w205 <w205@ip-172-31-20-97.ec2.internal>
2017-04-20 02:21:38 +0000

    transforming sql

--- exercise_1/transforming/transforming.sql
+++ exercise_1/transforming/transforming.sql
@@ -1,11 +1,11 @@
-DROP TABLE hospitals_x;
-CREATE TABLE hospitals_x
+DROP TABLE hospitals_parquet;
+CREATE TABLE hospitals_parquet
 AS SELECT *
 FROM hospitals;
 
 
-DROP TABLE effective_care_x;
-CREATE TABLE effective_care_x 
+DROP TABLE effective_care_parquet;
+CREATE TABLE effective_care_parquet 
 AS SELECT provider_id, 
 	hospital_name, 
 	score 
@@ -14,8 +14,8 @@ FROM effective_care;
 ALTER TABLE effective_care_x CHANGE score score INT;
 
 
-DROP TABLE readmissions_x;
-CREATE TABLE readmissions_x 
+DROP TABLE readmissions_parquet;
+CREATE TABLE readmissions_parquet 
 AS SELECT * 
 FROM readmissions;
 
@@ -24,13 +24,13 @@ ALTER TABLE readmissions_x CHANGE lower_estimate lower_estimate decimal(10,1);
 ALTER TABLE readmissions_x CHANGE higher_estimate higher_estimate decimal(10,1);
 
 
-DROP TABLE measures_x;
-CREATE TABLE measures_x 
+DROP TABLE measures_parquet;
+CREATE TABLE measures_parquet 
 AS SELECT * 
 FROM measures;
 
-DROP TABLE surveys_responses_x;
-CREATE TABLE surveys_responses_x 
+DROP TABLE surveys_responses_parquet;
+CREATE TABLE surveys_responses_parquet 
 AS SELECT * 
 FROM surveys_responses;
 
